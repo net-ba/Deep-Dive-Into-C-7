6d380ff6dff0776c13df59c6daeaa721da06ff4d
Jakub Šturc <jakub.sturc@erni.sk>
2018-05-23 16:44:31 +0200

    Rename and refactor ExpressionBodiedMembers

---
+++ examples/ExpressionBodiedMembersExample.cs
@@ -0,0 +1,23 @@
+﻿namespace JakubSturc.CSharp7DeepDive.Examples
+{
+    public class ExpressionBodiedMembersExample
+    {
+        private class Example
+        {
+            private string _name;
+
+            // available in C# 6.0
+            public int Getter => 42;
+            public void Method() => _name = _name.ToUpperInvariant();
+            // new in C# 7.0
+            public string Name
+            {
+                get => _name;
+                set => _name = value;
+            }
+            public char this[int i] => _name[i];
+            public Example(string name) => _name = name;
+            ~Example() => _name = null;
+        }
+    }
+}
